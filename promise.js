c84220d1a89f8ff0aecec82f5a841fb17016ca05
Tito Cahyo <tito.cahyo@gmail.com>
2022-06-13 21:06:21 +0700

    add : promise yang membaca parameter emosi

--- promise.js
+++ promise.js
@@ -1,7 +1,38 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  new Promise((resolve, reject) => {
+    if (emosi === null) {
+      return reject("data kosong");
+    } else {
+      return resolve(emosi);
+    }
+  });
+
+  const main = async () => {
+    try {
+      let theater1 = await promiseTheaterIXX();
+      let theater2 = await promiseTheaterVGC();
+      let hasilAkhir = [];
+      theater1.forEach((element) => {
+        if (element.hasil === emosi) {
+          hasilAkhir.push(element.namaFilm);
+        }
+      });
+      theater2.forEach((element) => {
+        if (element.hasil === emosi) {
+          hasilAkhir.push(element.namaFilm);
+        }
+      });
+      return hasilAkhir.length;
+    } catch (err) {
+      console.log("error");
+    }
+  };
+
+  return main();
+};
 
 module.exports = {
   promiseOutput,
